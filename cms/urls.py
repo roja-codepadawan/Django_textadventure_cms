26548b2182ba514612df4cf469da4dade504a369
Ron Rupp <92783992+roja-codepadawan@users.noreply.github.com>
2025-04-25 13:17:17 +0200

    story_list

--- cms/urls.py
+++ cms/urls.py
@@ -1,7 +1,9 @@
-# cms/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.story_list, name='story_list'),
+    path('play/<int:story_id>/', views.story_start, name='story_start'),
+    path('chapter/<int:chapter_id>/', views.show_chapter, name='show_chapter'),
 ]
+
--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from .models import Story
+from django.shortcuts import render, get_object_or_404
+from .models import Story, Chapter, Choice
 
-def home(request):
+
+def story_list(request):
     stories = Story.objects.all()
-    return render(request, 'home.html', {'stories': stories})
+    print("Gefundene Storys:", stories)
+    return render(request, 'play/story_list.html', {'stories': stories})
+
+
+def story_start(request, story_id):
+    story = get_object_or_404(Story, id=story_id)
+    start_chapter = story.chapter_set.filter(is_start=True).first()
+    return render(request, "play/chapter.html", {"chapter": start_chapter})
+
+
+def show_chapter(request, chapter_id):
+    chapter = get_object_or_404(Chapter, id=chapter_id)
+    return render(request, "play/chapter.html", {"chapter": chapter})
